43e2fe721c4a70e4a2bdacdeaff76c7bee66a483
andres <dandresleon64@gmail.com>
2024-08-20 09:31:37 -0500

    consulta inicial

--- src/infrastructure/database/repositories/business/auth_repository.py
+++ src/infrastructure/database/repositories/business/auth_repository.py
@@ -4,6 +4,7 @@ from src.core.enums.layer import LAYER
 from src.core.models.config import Config
 from src.core.wrappers.execute_transaction import execute_transaction
 from src.domain.models.business.auth.auth_login_request import AuthLoginRequest
+from src.domain.models.entities.user_location_rol.user_location_rol import UserLocationRol
 from src.domain.services.repositories.business.i_auth_repository import IAuthRepository
 from src.infrastructure.database.entities.company_entity import CompanyEntity
 from src.infrastructure.database.entities.currency_entity import CurrencyEntity
@@ -11,9 +12,11 @@ from src.infrastructure.database.entities.currency_location_entity import (
     CurrencyLocationEntity,
 )
 from src.infrastructure.database.entities.language_entity import LanguageEntity
+from src.infrastructure.database.entities.location_entity import LocationEntity
 from src.infrastructure.database.entities.platform_entity import PlatformEntity
 from src.infrastructure.database.entities.rol_entity import RolEntity
 from src.infrastructure.database.entities.user_entity import UserEntity
+from src.infrastructure.database.entities.user_location_rol_entity import UserLocationRolEntity
 
 
 class AuthRepository(IAuthRepository):
@@ -31,27 +34,28 @@ class AuthRepository(IAuthRepository):
     ]:
         db = config.db
 
-        results = (
+        """ results = (
             db.query(
-                UserEntity,
-                RolEntity,
                 PlatformEntity,
+                UserEntity,
                 LanguageEntity,
+                LocationEntity,
                 CurrencyLocationEntity,
                 CurrencyEntity,
+                UserLocationRolEntity,
+                RolEntity
             )
-            .join(UserEntity, UserEntity.rol_id == RolEntity.id)
             .join(PlatformEntity, PlatformEntity.id == UserEntity.platform_id)
             .join(LanguageEntity, LanguageEntity.id == PlatformEntity.language_id)
-            .join(
-                CurrencyLocationEntity,
-                CurrencyLocationEntity.id == PlatformEntity.currency_location_id,
-            )
-            .join(
-                CurrencyEntity, CurrencyEntity.id == CurrencyLocationEntity.currency_id
-            )
-            .filter(UserEntity.email == params.email)
+            .join(LocationEntity, LocationEntity.id == PlatformEntity.location_id)
+            .join(CurrencyLocationEntity, CurrencyLocationEntity.id == PlatformEntity.currency_location_id)
+            .join(CurrencyEntity, CurrencyEntity.id == CurrencyLocationEntity.currency_id)
+            .join(UserLocationRolEntity, UserLocationRolEntity.user_id == UserEntity.id)
+            .join(UserLocationRolEntity, UserLocationRolEntity.rol_id == RolEntity.id)
+            .join(UserLocationRolEntity, UserLocationRolEntity.location_id == LocationEntity.id)
+            .filter(UserEntity, UserEntity.email == params.email)
             .first()
-        )
+        ) """
 
-        return results
+        pass
+
