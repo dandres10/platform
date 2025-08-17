a3fccd2025ac64ed636d5798bc566851375bed8f
andres <dandresleon64@gmail.com>
2025-08-17 13:48:14 -0500

    refresh token

--- src/domain/models/business/auth/refresh_token/auth_refresh_token_response.py
+++ src/domain/models/business/auth/refresh_token/auth_refresh_token_response.py
@@ -1,5 +1,11 @@
 from pydantic import BaseModel, Field
+from src.domain.models.business.auth.login.auth_login_response import (
+    PlatformConfiguration,
+    PlatformVariations,
+)
 
 
 class AuthRefreshTokenResponse(BaseModel):
+    platform_configuration: PlatformConfiguration = Field(...)
+    platform_variations: PlatformVariations = Field(...)
     token: str = Field(...)
